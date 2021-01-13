8276878c89059dc7c7cef056800b1258ed8d915c
joao.borba <jptruchinski@gmail.com>
2021-01-12 22:36:54 -0300

    Criação da EditServlet.java

--- src/main/java/servlet/properties/EditServlet.java
+++ src/main/java/servlet/properties/EditServlet.java
@@ -1,4 +1,28 @@
 package servlet.properties;
 
-public class EditServlet {
+import model.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class EditServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String key = req.getParameter("key");
+        if (key != null)
+        {
+            String value = Properties.getValue(key);
+            if (value != null) {
+                req.setAttribute("key", key);
+                req.setAttribute("value", value);
+            }
+        }
+
+        req.getRequestDispatcher("/edit.jsp").forward(req, resp);
+
+    }
 }
