522b8f7687c008d2688a4a107f7dc89d71666b57
joao.borba <jptruchinski@gmail.com>
2021-01-12 22:46:30 -0300

    Criação do servlet SaveServlet

--- src/main/java/servlet/properties/SaveServlet.java
+++ src/main/java/servlet/properties/SaveServlet.java
@@ -1,4 +1,21 @@
 package servlet.properties;
 
-public class SaveServlet {
+import model.Properties;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SaveServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String key = req.getParameter("key");
+        String value = req.getParameter("value");
+
+        Properties.save(key, value);
+
+        resp.sendRedirect("/Props/List");
+    }
 }
