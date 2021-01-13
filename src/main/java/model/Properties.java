d1a2a6e4baa4e4320fb14f6a120dcc15d9b36c99
joao.borba <jptruchinski@gmail.com>
2021-01-12 21:50:30 -0300

    Criação da classe Properties.java

--- src/main/java/model/Properties.java
+++ src/main/java/model/Properties.java
@@ -1,4 +1,57 @@
 package model;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Properties {
+    private static final String FILE_NAME = "d:/temp/JavaWeb/props.txt"; //carrega o arquivo de propriedades na memória
+    private static java.util.Properties props; //permite mappear chave à valores.
+
+    static
+    {
+        //executado quando a classe é carregada pela primeira vez na JVM
+        try{
+            props = new java.util.Properties();
+            props.load(new FileInputStream(FILE_NAME));
+        } catch (Exception e ){
+            e.printStackTrace();
+        }
+
+    }
+
+    private Properties() {
+
+    }
+
+    public static Map<String, String> getProperties() {
+
+        Map<String, String> map = new TreeMap<String, String>();
+        Enumeration<Object> e = props.keys();
+
+        while (e.hasMoreElements()) {
+            String key = (String) e.nextElement();
+            String value = props.getProperty(key);
+            map.put(key, value);
+        }
+
+        return map;
+    }
+
+    public static void delete (String key) throws IOException{
+        props.remove(key);
+        props.store(new FileOutputStream(FILE_NAME), ""); //GRAVAÇÃO dos dados atualizados no arquivo (cria novamente o arquivo, mas atualizado)
+    }
+
+    public static void save(String key, String value) throws IOException{
+        props.setProperty(key, value);
+        props.store(new FileOutputStream(FILE_NAME), "");
+    }
+
+    public static String getValue(String key){
+        return props.getProperty(key);
+    }
 }
