85837a7ef16da1d3724b49d40dbef5522d96f3b4
joao.borba <jptruchinski@gmail.com>
2021-01-14 22:48:19 -0300

    Criação da classe de config

---
+++ src/main/java/entity/Config.java
@@ -0,0 +1,10 @@
+package entity;
+
+public class Config {
+    private String patternApp = "http://localhost:8081/training_servlet_oficial_war_exploded/";
+
+    public String getPatternApp()
+    {
+        return this.patternApp;
+    }
+}
